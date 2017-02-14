37707add9424581a5220d5473d91e7145e48080d
Sarada1987 <sarada1987@gmail.com>
2017-02-14 14:33:06 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
 //move the image
-var img = document.getElementByID('img');
+var img = document.getElementByID('madi');
 img.onClick = function(){
     img.style.marginLeft = '100px';
 }
