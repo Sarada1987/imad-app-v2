a2118fe330bce2465e2d36642df5979fa5ac6a2d
Sarada1987 <sarada1987@gmail.com>
2017-02-14 17:19:56 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,8 +19,7 @@ button.onclick = function (){
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn')
 submit.onclick = function() {
     //make a request to the server and send the name
@@ -43,6 +42,8 @@ submit.onclick = function() {
             }
         }
     };
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
   request.open('GET', 'http://sarada1987.imad.hasura-app.io/submit-name?name='+name,true)
   request.send(null);
     
