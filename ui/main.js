86d2f399d8fe6be372ab5fa88d79e331c874b6b3
Sarada1987 <sarada1987@gmail.com>
2017-02-14 16:43:21 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,19 @@ button.onclick = function (){
   request.open('GET', 'http://sarada1987.imad.hasura-app.io/counter',true)
   request.send(null);
 };
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn')
+submit.click = function() {
+    //make a request to the server and send the name
+    //capture a list of names and render it as a list
+    var names = ['name1', 'name2', 'name3'];
+    var list='';
+    for  (var i=0; i<names.length; i++){
+        list += '<li>'+names[i]+'</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+}
