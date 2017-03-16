99b3d573ec6f8a8b47aa03db9ba5bd9a4ff371e6
Sarada1987 <sarada1987@gmail.com>
2017-03-16 15:09:17 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,17 +9,18 @@ submit.onclick = function() {
     // Capture the response and store it in a variable
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
             if (request.status === 200){
                 //capture a list of names and render it as a list
                 console.log('user logged in');
                 alert('Logged in successfully');
-            }
-            } else if(request.status === 403){
+            }else if(request.status === 403){
                 alert('Username/password is incorrect');
                 
             }else if (request.status === 500){
                 alert('Something went wrong on the server')
             }
+        }    
 
     };
   var username = document.getElementById('username').value;
